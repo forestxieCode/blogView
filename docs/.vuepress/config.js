8d7a47412626be5ae60260f734a461f4a4408751
xiesenlin <1397798719@qq.com>
2021-04-02 14:00:34 +0800

    fix:修改根目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const themeConfig = require('./config/theme/')
 
 module.exports = {
   title: "Forest Xie",
+  base: '/blogView/',   // 设置站点根路径
   description: '我追。一个成年人在一群尖叫的孩子中奔跑，但我不在乎。我追。风拂过我的脸庞，我唇上挂着一个像潘杰希尔峡谷那样大大的微笑。',
   dest: 'public',
   head: [
