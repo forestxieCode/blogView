c622be0772a8420096707a2342dfd93494c72337
xiesenlin <1397798719@qq.com>
2021-04-02 15:32:50 +0800

    faet:添加标题以及动态监听文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const themeConfig = require('./config/theme/')
 
 module.exports = {
-  title: "Forest Xie",
+  title: "Forest `Blog",
   base: '/blogView/',   // 设置站点根路径
   description: '我追。一个成年人在一群尖叫的孩子中奔跑，但我不在乎。我追。风拂过我的脸庞，我唇上挂着一个像潘杰希尔峡谷那样大大的微笑。',
   dest: 'public',
@@ -14,5 +14,24 @@ module.exports = {
   markdown: {
     lineNumbers: true
   },
-  plugins: ['@vuepress/medium-zoom', 'flowchart']
+  plugins: ['@vuepress/medium-zoom', 'flowchart',
+    [
+      "cursor-effects",
+      {
+        size: 2,                    // size of the particle, default: 2
+        shape: ['circle'],  // shape of the particle, default: 'star'， 可选'circle'
+        zIndex: 999999999           // z-index property of the canvas, default: 999999999
+      }
+    ],
+    [
+      "dynamic-title",
+      {
+        showIcon: "/favicon.ico",
+        showText: "风筝回来拉!",
+        hideIcon: "/failure.ico",
+        hideText: "风筝飞走拉!",
+        recoverTime: 2000
+      }
+    ]
+  ]
 }
